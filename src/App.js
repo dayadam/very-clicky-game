addffa5090d00316a50b470c2db298bcb2f08a17
dayadamuga@gmail.com <dayadamuga@gmail.com>
2020-01-01 17:00:49 -0500

    rendering from a map function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
-import React from "react";
-import Card from "./components/Card";
+import React, { Component } from "react";
+//import Card from "./components/Card";
+import Skateboard from "./components/images/Skateboard.svg";
+import Bike from "./components/images/Bike.svg";
+import Surfer from "./components/images/Surfer.svg";
+import HammerThrow from "./components/images/Hammer-throw.svg";
+import Runner from "./components/images/Runner.svg";
+import Muscle from "./components/images/Muscle.svg";
+import Soccer from "./components/images/Soccer.svg";
+import Baseball from "./components/images/Baseball.svg";
+import Motocross from "./components/images/Motocross.svg";
+//import Diver from "./components/images/Diver.svg";
+import IceSkating from "./components/images/Ice-skating.svg";
+import Stretching from "./components/images/Stretching.svg";
+import Steeplechase from "./components/images/Steeplechase.svg";
+import "./components/Card.css";
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <Card />
+const images = [
+  { image: Skateboard, name: "Skateboard" },
+  { image: Bike, name: "Bike" },
+  { image: Surfer, name: "Surfer" },
+  { image: HammerThrow, name: "HammerThrow" },
+  { image: Soccer, name: "Soccer" },
+  { image: Muscle, name: "Muscle" },
+  { image: Runner, name: "Runner" },
+  { image: Baseball, name: "Baseball" },
+  { image: Motocross, name: "Motocross" },
+  { image: IceSkating, name: "IceSkating" },
+  { image: Stretching, name: "Stretching" },
+  { image: Steeplechase, name: "Steeplechase" }
+];
+
+class App extends Component {
+  state = {
+    score: 0,
+    images
+  };
+
+  handleClick = event => {};
+
+  render() {
+    return (
+      <div className="background">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>Clicky game! Score</h1>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            {this.state.images.map(image => (
+              <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                <img src={image.image} alt={image.name} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -20,50 +20,53 @@ const style = {
   }
 };
 
+const cards = [
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Skateboard} alt="Skateboard" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Bike} alt="Bike" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={IceSkating} alt="IceSkating" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Motocross} alt="Motocross" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Steeplechase} alt="Steeplechase" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Soccer} alt="Soccer" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Baseball} alt="Baseball" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Surfer} alt="Surfer" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={HammerThrow} alt="HammerThrow" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Runner} style={style.sizing} alt="Runner" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Muscle} alt="Muscle" />
+  </div>,
+  <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+    <img src={Stretching} alt="Stretching" />
+  </div>
+];
+
+const cardLength = cards.length
+
+const randomCards = cards.map(card => {
+
+})
+
 function Card() {
-  return (
-    <>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Skateboard} alt="Skateboard" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Bike} alt="Bike" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={IceSkating} alt="IceSkating" />
-      </div>
-{/*       <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Diver} alt="Diver" />
-      </div> */}
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Motocross} alt="Motocross" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Steeplechase} alt="Steeplechase" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Soccer} alt="Soccer" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Baseball} alt="Baseball" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Surfer} alt="Surfer" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={HammerThrow} alt="HammerThrow" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Runner} style={style.sizing} alt="Runner" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Muscle} alt="Muscle" />
-      </div>
-      <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
-        <img src={Stretching} alt="Stretching" />
-      </div>
-    </>
-  );
+  return <>{cards}</>;
 }
 
 export default Card;
