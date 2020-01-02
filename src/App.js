b7daa0f979b3d2e4d7c5b5460ced8a04b3733601
dayadamuga@gmail.com <dayadamuga@gmail.com>
2020-01-01 19:16:06 -0500

    click counter working

--- src/App.js
+++ src/App.js
@@ -16,27 +16,35 @@ import Steeplechase from "./components/images/Steeplechase.svg";
 import "./components/Card.css";
 
 const images = [
-  { image: Skateboard, name: "Skateboard" },
-  { image: Bike, name: "Bike" },
-  { image: Surfer, name: "Surfer" },
-  { image: HammerThrow, name: "HammerThrow" },
-  { image: Soccer, name: "Soccer" },
-  { image: Muscle, name: "Muscle" },
-  { image: Runner, name: "Runner" },
-  { image: Baseball, name: "Baseball" },
-  { image: Motocross, name: "Motocross" },
-  { image: IceSkating, name: "IceSkating" },
-  { image: Stretching, name: "Stretching" },
-  { image: Steeplechase, name: "Steeplechase" }
+  { image: Skateboard, id: 1, clicked: false, name: "Skateboard" },
+  { image: Bike, id: 2, clicked: false, name: "Bike" },
+  { image: Surfer, id: 3, clicked: false, name: "Surfer" },
+  { image: HammerThrow, id: 4, clicked: false, name: "HammerThrow" },
+  { image: Soccer, id: 5, clicked: false, name: "Soccer" },
+  { image: Muscle, id: 6, clicked: false, name: "Muscle" },
+  { image: Runner, id: 7, clicked: false, name: "Runner" },
+  { image: Baseball, id: 8, clicked: false, name: "Baseball" },
+  { image: Motocross, id: 9, clicked: false, name: "Motocross" },
+  { image: IceSkating, id: 10, clicked: false, name: "IceSkating" },
+  { image: Stretching, id: 11, clicked: false, name: "Stretching" },
+  { image: Steeplechase, id: 12, clicked: false, name: "Steeplechase" }
 ];
 
 class App extends Component {
   state = {
+    clicked: [],
     score: 0,
     images
   };
 
-  handleClick = event => {};
+  handleClick = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const clicked = this.state.clicked.concat(id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ clicked });
+  };
+
+  //handleClick = event => {};
 
   render() {
     return (
@@ -44,14 +52,19 @@ class App extends Component {
         <div className="container">
           <div className="row">
             <div className="col">
-              <h1>Clicky game! Score</h1>
+              <h1>Clicky game! Score: {this.state.clicked.length}</h1>
             </div>
           </div>
         </div>
         <div className="container">
           <div className="row">
             {this.state.images.map(image => (
-              <div className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3">
+              <div
+                onClick={() => this.handleClick(image.id)}
+                key={image.id}
+                id={image.id}
+                className="col col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3"
+              >
                 <img src={image.image} alt={image.name} />
               </div>
             ))}
