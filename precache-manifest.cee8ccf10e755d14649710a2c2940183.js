a2e10da95ac812e77f387e3020dff5888cec5759
dayadam <dayadamuga@gmail.com>
2020-01-03 12:40:43 -0500

    Updates

---
+++ precache-manifest.cee8ccf10e755d14649710a2c2940183.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c86d2f7852b0a6bad7cd58499d61361",
+    "url": "/very-clicky-game/index.html"
+  },
+  {
+    "revision": "a79bd6b11fd5f9930a3a",
+    "url": "/very-clicky-game/static/css/main.6c51ed73.chunk.css"
+  },
+  {
+    "revision": "58179d69ff1b3afff845",
+    "url": "/very-clicky-game/static/js/2.86c35142.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/very-clicky-game/static/js/2.86c35142.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a79bd6b11fd5f9930a3a",
+    "url": "/very-clicky-game/static/js/main.58874a02.chunk.js"
+  },
+  {
+    "revision": "752d51f13863cd2db23e",
+    "url": "/very-clicky-game/static/js/runtime-main.53807d5a.js"
+  },
+  {
+    "revision": "125cc5cf969dfb2591ee6949163a9a36",
+    "url": "/very-clicky-game/static/media/Baseball.125cc5cf.svg"
+  },
+  {
+    "revision": "ce9298056c44e35322490ab3798c431e",
+    "url": "/very-clicky-game/static/media/Bike.ce929805.svg"
+  },
+  {
+    "revision": "03d5b3a16d5930c97b1a229aed1bbc3a",
+    "url": "/very-clicky-game/static/media/Hammer-throw.03d5b3a1.svg"
+  },
+  {
+    "revision": "d25922e3cd33e1fa9c7f89a458ab595b",
+    "url": "/very-clicky-game/static/media/Ice-skating.d25922e3.svg"
+  },
+  {
+    "revision": "33caf8f9872e10c899d9e332497ec9ca",
+    "url": "/very-clicky-game/static/media/Motocross.33caf8f9.svg"
+  },
+  {
+    "revision": "4854dd77bd40584508954fc68709b2cc",
+    "url": "/very-clicky-game/static/media/Muscle.4854dd77.svg"
+  },
+  {
+    "revision": "bac6c4775488d6da0abdcb0a908f52c5",
+    "url": "/very-clicky-game/static/media/Runner.bac6c477.svg"
+  },
+  {
+    "revision": "a6549f49b401739abdb5584e20a00084",
+    "url": "/very-clicky-game/static/media/Skateboard.a6549f49.svg"
+  },
+  {
+    "revision": "882ebf75e8b26efd654b319e4581ee87",
+    "url": "/very-clicky-game/static/media/Soccer.882ebf75.svg"
+  },
+  {
+    "revision": "7258bcb21f3c71986be9054ef9ad0be9",
+    "url": "/very-clicky-game/static/media/Steeplechase.7258bcb2.svg"
+  },
+  {
+    "revision": "682c9dc70a7969d68bf8db69a4f62fb0",
+    "url": "/very-clicky-game/static/media/Stretching.682c9dc7.svg"
+  },
+  {
+    "revision": "af30d77d59aad8a8320da8f6e38d8004",
+    "url": "/very-clicky-game/static/media/Surfer.af30d77d.svg"
+  }
+]);
